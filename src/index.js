a0e5659bbeeea06b2bbdd519c7ee70c103775321
Miloš Milutinović <milosmilutinovic@elfak.rs>
2018-04-09 19:55:20 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -2,49 +2,215 @@ import './style.css';
 import {TaskServices} from './task.services';
 import {Task} from './task';
 import * as Rxjs from 'rxjs';
+import img from './avatar.png';
+
 
 
 const main = document.getElementById('main');
 
-//#region crtanje taskova
+//#region crtanje forme za kreiranje novog naloga
 
-const nacrtaj = document.createElement('button');
-nacrtaj.textContent = 'Nacrtaj';
-main.appendChild(nacrtaj);
-const div = document.createElement('div');
-main.appendChild(div);
+// const createAccountBox = document.createElement('div');
+// createAccountBox.classList.add('login-box');
+// main.appendChild(createAccountBox);
 
-const obCrtanje = Rxjs.Observable.fromEvent(nacrtaj, 'click')
+// const logo = document.createElement('img');
+// logo.src = img;
+// logo.className = 'avatar';
+// createAccountBox.appendChild(logo);
+
+// const naslov = document.createElement('h1');
+// naslov.textContent = 'Create new account';
+// createAccountBox.appendChild(naslov);
+
+// const p = document.createElement('p');
+// p.textContent = 'Username';
+// createAccountBox.appendChild(p);
+
+// const username = document.createElement('input');
+// username.type = 'text';
+// username.placeholder = 'Enter Username';
+// createAccountBox.appendChild(username);
+
+// const p1 = document.createElement('p');
+// p1.textContent = 'Password';
+// createAccountBox.appendChild(p1);
+
+// const password = document.createElement('input');
+// password.type = 'password';
+// password.placeholder = 'Enter Password';
+// createAccountBox.appendChild(password);
+
+// const create = document.createElement('input');
+// create.type = 'submit';
+// create.value = 'Create';
+// create.className = 'login-button';
+// createAccountBox.appendChild(create);
+
+// const link = document.createElement('a');
+// link.href = '#';
+// link.textContent = 'Alredy have account?';
+// createAccountBox.appendChild(link);
+
+// const obCreate = Rxjs.Observable.fromEvent(create, 'click')
+//     .subscribe(
+//         () =>{
+//             fetch(`http://localhost:3000/users?id=${username.value}`)
+//                 .then(response => response.json())
+//                 .then(data => {
+//                     if(data.length != 0) {
+//                         window.alert('That username alredy exist!');
+//                     }
+//                     else {
+//                         fetch('http://localhost:3000/users', {
+//                             method: 'POST',
+//                             headers: {
+//                                 'Content-type': 'application/json; charset=UTF-8'
+//                             },
+//                             body: JSON.stringify({ tasks: [], password: password.value, id: username.value})
+//                         }).then(res => console.log(res));
+//                     }
+//                 })
+//         }
+//     );
+
+//endregion
+
+//#region crtanje login forme
+
+const loginBox = document.createElement('div');
+loginBox.classList.add('login-box');
+main.appendChild(loginBox);
+
+const logo = document.createElement('img');
+logo.src = img;
+logo.className = 'avatar';
+loginBox.appendChild(logo);
+
+const naslov = document.createElement('h1');
+naslov.textContent = 'Login Here';
+loginBox.appendChild(naslov);
+
+const p = document.createElement('p');
+p.textContent = 'Username';
+loginBox.appendChild(p);
+
+const username = document.createElement('input');
+username.type = 'text';
+username.placeholder = 'Enter Username';
+loginBox.appendChild(username);
+
+const p1 = document.createElement('p');
+p1.textContent = 'Password';
+loginBox.appendChild(p1);
+
+const password = document.createElement('input');
+password.type = 'password';
+password.placeholder = 'Enter Password';
+loginBox.appendChild(password);
+
+const login = document.createElement('button');
+login.textContent = 'Login';
+login.className = 'login-button';
+loginBox.appendChild(login);
+
+const link = document.createElement('a');
+link.href = '#';
+link.textContent = 'Create new account';
+loginBox.appendChild(link);
+
+const obLogin = Rxjs.Observable.fromEvent(login, 'click')
     .subscribe(
         () => {
-            div.innerHTML = '';
-            TaskServices.getTasksFromServer()
+            fetch(`http://localhost:3000/users?id=${username.value}&password=${password.value}`)
+                .then(response => response.json())
                 .then(data => {
-                    data.forEach((task)=> {
-                        const box = document.createElement('div');
-                        box.classList.add('task');
-                        div.appendChild(box);
-                        const h3 = document.createElement('h3');
-                        h3.innerHTML = task.id;
-                        box.appendChild(h3);
-                        const p = document.createElement('p');
-                        p.innerHTML = task.desc;
-                        box.appendChild(p);
-                        const obrisi = document.createElement('button');
-                        obrisi.textContent = 'X';
-                        box.appendChild(obrisi);
-                    });
+                    if(data.length == 0) {
+                        window.alert('Wrong username or password!');
+                    }
+                    else {
+                        console.log(data);
+                        nacrtajUsera(main, data[0]);
+                    }
                 })
         }
     );
-const obObrisi = Rxjs.Observable.fromEvent(document, 'click')
-    .filter((e)=> e.target.textContent == 'X')
-    .pluck('target', 'parentNode', 'firstChild', 'textContent')
-    .subscribe(
-        (taskName) => {
-            TaskServices.deleteTaskFromServer(taskName);//implementiraj kasnije da se auto refresuje..
-        }
-    )
+
+
+//endregion
+
+//#region crtanje usera
+
+const nacrtajUsera = (host, user) => {
+    host.innerHTML = '';
+    const naslov = document.createElement('h1');
+    naslov.textContent = user.id;
+    console.log(user.id);
+    host.appendChild(naslov);
+    user.tasks.forEach((task)=> {
+        const box = document.createElement('div');
+        box.classList.add('task');
+        host.appendChild(box);
+        const h3 = document.createElement('h3');
+        h3.innerHTML = task.id;
+        box.appendChild(h3);
+        const p = document.createElement('p');
+        p.innerHTML = task.desc;
+        box.appendChild(p);
+        const obrisi = document.createElement('button');
+        obrisi.textContent = 'X';
+        box.appendChild(obrisi);
+    });
+
+    
+}
+
+//endregion
+
+//#region crtanje taskova
+
+// const nacrtaj = document.createElement('button');
+// nacrtaj.textContent = 'Nacrtaj';
+// main.appendChild(nacrtaj);
+// const div = document.createElement('div');
+// main.appendChild(div);
+
+// const obCrtanje = Rxjs.Observable.fromEvent(nacrtaj, 'click')
+//     .subscribe(
+//         () => {
+//             div.innerHTML = '';
+//             TaskServices.getTasksFromServer()
+//                 .then(data => {
+//                     data.forEach((task)=> {
+//                         const box = document.createElement('div');
+//                         box.classList.add('task');
+//                         div.appendChild(box);
+//                         const h3 = document.createElement('h3');
+//                         h3.innerHTML = task.id;
+//                         box.appendChild(h3);
+//                         const p = document.createElement('p');
+//                         p.innerHTML = task.desc;
+//                         box.appendChild(p);
+//                         const obrisi = document.createElement('button');
+//                         obrisi.textContent = 'X';
+//                         box.appendChild(obrisi);
+//                     });
+//                 })
+//         }
+//     );
+
+//endregion
+
+//#region logika za brisanje taska iz baze
+
+// const obObrisi = Rxjs.Observable.fromEvent(document, 'click')
+//     .filter((e)=> e.target.textContent == 'X')
+//     .pluck('target', 'parentNode', 'firstChild', 'textContent')
+//     .subscribe(
+//         (taskName) => {
+//             TaskServices.deleteTaskFromServer(taskName);//implementiraj kasnije da se auto refresuje..
+//         }
+//     )
 
 //endregion
 
@@ -85,30 +251,5 @@ const obObrisi = Rxjs.Observable.fromEvent(document, 'click')
 //             })
 //         }
 //     );
-
-//endregion
-
-//#region stari kod za usera
-// const u = new User();
-
-
-// const div = document.createElement('div');
-// document.body.appendChild(div);
-// const name = document.createElement('input');
-// name.type = 'text';
-// name.placeholder = 'Task';
-// div.appendChild(name);
-// const desc = document.createElement('input');
-// desc.type = 'text'
-// desc.placeholder = "Description";
-// div.appendChild(desc);
-// const dugme = document.createElement('button');
-// dugme.textContent = 'Dodaj';
-// dugme.onclick = () => {
-//     t.addNewTaskToServer(new Task(undefined, name.value, desc.value));
-
-//     t.draw();
-// }
-// div.appendChild(dugme);
 
 //endregion
---
+++ src/user.js
@@ -0,0 +1,8 @@
+
+export class User {
+    constructor(username, password, tasks) {
+        this.username = username;
+        this.password = password;
+        this.tasks = tasks;
+    }
+}
